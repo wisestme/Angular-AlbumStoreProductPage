69051a24efb62ca4d04601a5ddca48c4b4c3a3f9
wisestMe <wyhzest@gmail.com>
2019-11-03 00:37:41 +0100

    creates a private class property

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
