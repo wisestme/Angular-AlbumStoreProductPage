d0be248c485f3dff0b942835e75bbfc4d7352e87
wisestMe <wyhzest@gmail.com>
2019-11-03 00:39:37 +0100

    adds a method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,7 +4,7 @@ import { Http, Response } from '@angular/http';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-
+  getAlbum(id:number);
   constructor(private _http: Http) { }
 
 }
