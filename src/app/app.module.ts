bfe1c90d2765732cf5291534552ad8dd1872824a
wisestMe <wyhzest@gmail.com>
2019-11-03 00:25:38 +0100

    adds class to providers array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { ProductService } from './product.service'
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }s
