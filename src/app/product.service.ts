2ded69663c0718202b8317d7689f6fb2851303a7
wisestMe <wyhzest@gmail.com>
2019-11-03 00:49:04 +0100

    maps json response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import rxjs/add/operator/map
 
 @Injectable()
 export class ProductService {
@@ -8,6 +9,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(_albumUrl);
+    return this._http.get(_albumUrl).response.json();
   };
 }
