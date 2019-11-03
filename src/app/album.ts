2ad6c17d8003ec117f58a46f9b8e067e8fa38668
wisestMe <wyhzest@gmail.com>
2019-11-03 16:25:46 +0100

    creates and imports interfaces

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,2 +1,8 @@
+import { track } from './track';
+
 export interface Album {
+  name: string;
+  releaseDate: string;
+  coverImage: string;
+  tracks: Track[];
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { album } from './album';
 
 @Injectable()
 export class ProductService {
