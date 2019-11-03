1161cf5009653a240443c2819422b349397f57be
wisestMe <wyhzest@gmail.com>
2019-11-03 01:13:23 +0100

    injects a private instance of a service

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _productService) { }
 
   ngOnInit() {
   }
