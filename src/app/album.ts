a6d9f3132cb91f25dc031a7123a2264b951adeea
wisestMe <wyhzest@gmail.com>
2019-11-03 17:11:04 +0100

    adds return type to method

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,4 +1,4 @@
-import { track } from './track';
+import { Track } from './track';
 
 export interface Album {
   name: string;
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -9,7 +10,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number){
+  getAlbum(id: number): Observable<album> {
     return this._http.get(this._albumUrl).map((response) => response.json());
   };
 }
