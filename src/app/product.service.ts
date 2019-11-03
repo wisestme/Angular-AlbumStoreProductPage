f56ffad2ddca692b7c2e9481ba922750e9cb97eb
wisestMe <wyhzest@gmail.com>
2019-11-03 17:23:55 +0100

    adds type to property

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,6 +11,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => response.json(): Album);
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   };
 }
