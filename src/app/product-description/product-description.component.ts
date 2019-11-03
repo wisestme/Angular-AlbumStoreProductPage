d1657bd64be059d27f835a4b9c7c8ecf5b679594
wisestMe <wyhzest@gmail.com>
2019-11-03 01:16:13 +0100

    removes omission error

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor(private _productService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
