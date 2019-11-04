9792f3563a354dc27f25e2634e719ebd2fab3c02
wisestMe <wyhzest@gmail.com>
2019-11-04 19:28:02 +0100

    creates method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -16,5 +16,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   };
 
-  getProducts();
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl)map((response) => <Product[]>response.json());
+  };
 }
