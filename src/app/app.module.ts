b78d7366048d9ad1c3e5b5f97c2c50a098b8b302
wisestMe <wyhzest@gmail.com>
2019-11-04 23:01:38 +0100

    removes error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component'
 
-import { RouterModule } from '@angular/router';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
 
 const appRoutes: Routes = [
   {
