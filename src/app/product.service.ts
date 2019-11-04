12675e036155f18f8a50c23ba05e3b3dd6b7706f
wisestMe <wyhzest@gmail.com>
2019-11-04 18:29:22 +0100

    creates method in interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,4 +15,6 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   };
+
+  getProducts();
 }
